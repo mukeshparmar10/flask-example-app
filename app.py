6ec30283716ae10e3bf426951db5cd89e0062369
mukeshparmar10 <mukeshparmar10@gmail.com>
2025-11-06 16:01:30 +0530

    Code and files update

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 from flask import Flask, request, Response, redirect, url_for, session, render_template
+import mysql.connector
+import os
+
+mydb = mysql.connector.connect(
+	host=os.environ.get("DB_HOST"),
+	user=os.environ.get("DB_USER"),
+	passwd=os.environ.get("DB_PASSWORD"),
+	database=os.environ.get("DB_DATABASE"),
+	port=int(os.environ.get("DB_PORT"))	
+)
+
+mycursor = mydb.cursor()
 
 app = Flask(__name__)
+app.secret_key="supersecret"
 
 @app.route("/")
-def index():
-	return render_template("index.html")
+def index():	
+	mycursor.execute("select * from student")
+	data = mycursor.fetchall()
+	return render_template("index.html",data=data)
+
 
 if __name__=="__main__":
 	app.run(host="0.0.0.0",debug=True)
