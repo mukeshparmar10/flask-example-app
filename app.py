47a1ab36351566e7c91331db94d1397e5bb566ee
mukeshparmar10 <mukeshparmar10@gmail.com>
2025-11-07 17:11:19 +0530

    Code update

--- app.py
+++ app.py
@@ -1,27 +1,30 @@
 from flask import Flask, request, Response, redirect, url_for, session, render_template
 import mysql.connector
 import os
+from waitress import serve
 
-mydb = mysql.connector.connect(
-	host=os.environ.get("DB_HOST"),
-	user=os.environ.get("DB_USER"),
-	passwd=os.environ.get("DB_PASSWORD"),
-	database=os.environ.get("DB_DATABASE"),
-	port=int(os.environ.get("DB_PORT"))	
-)
-
-mycursor = mydb.cursor()
+def connection():
+	mydb = mysql.connector.connect(
+		host=os.environ.get("DB_HOST"),
+		user=os.environ.get("DB_USER"),
+		passwd=os.environ.get("DB_PASSWORD"),
+		database=os.environ.get("DB_DATABASE"),
+		port=int(os.environ.get("DB_PORT"))	
+	)
+	return mydb
 
 app = Flask(__name__)
-app.secret_key="supersecret"
 
 @app.route("/")
-def index():	
+def index():
+	mydb = connection()
+	mycursor = mydb.cursor()
 	mycursor.execute("select * from student")
 	data = mycursor.fetchall()
-	return render_template("index.html",data=data)
+	mydb.close()
+	# return render_template("index.html",data=data)
+	return data
 
 
 if __name__=="__main__":
-	app.run(host="0.0.0.0",debug=True)
-
+	serve(app,host="0.0.0.0",threads=10)
